9f149e731ee1141fe239448060625eca59804fb4
adnanysf <asubyusuf@gmail.com>
2023-11-22 14:59:00 -0600

    added basic elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-
+import styles from './page.module.css'
 
 export default function Page() {
     return (
-        <div>
-            <h1>Page</h1>
+        <div className={styles.app}>
+          <div className={styles.content}>
+            <div className={styles.title}>
+              <span className={styles.intro}><div>Howdy! My name is</div></span>
+              <span className={styles.name}><div>Adnan Yusuf</div></span>
+              <span className={styles.introtwo}><div> and I'm a </div></span>
+              <span className={styles.pos}><div>Software Engineer</div></span>
+            </div>
+            <div className={styles.links}>
+              <div>LinkedIn</div>
+              <div>Github</div>
+              <div>About Me</div>
+
+            </div>
+          </div>
         </div>
     )
 }
