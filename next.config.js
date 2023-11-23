d4a6805198e26143b4e467047e99cd227b90de3c
adnanysf <asubyusuf@gmail.com>
2023-11-23 02:19:13 -0600

    using config file from next

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = {
-    // Other configurations...
-  
-    // Enable static HTML export
-    output: {
-      // Set the path for the exported files (default is 'out')
-      path: 'out',
-    },
-  };
+const nextConfig = {
+    output: 'export',
+   
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+   
+  module.exports = nextConfig
