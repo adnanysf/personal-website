ae75c8950ea33e1a91671e0121f0b99ac6562799
adnanysf <asubyusuf@gmail.com>
2025-03-13 23:11:47 -0500

    added photo, and other containers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,18 +11,40 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 
 export default function Page() {
-
+  const searchBarItems = ['Resume', 'Github', 'LinkedIn', 'Expertise', 'GPA', 'Photo'];
   return(
     <div className={styles.maincontainer}>
-      <div className={styles.header}>
+      <nav className={styles.header}>
+
         <a href='/Yusuf_Resume.pdf'>Resume</a>
         <a href='https://www.linkedin.com/in/adnyusuf/'>Github</a>
         <a href='https://github.com/adnanysf'>LinkedIn</a>
+      </nav>
+      
+        <div className={styles.contentContainer}>
+
+          <div className={styles.currentContainer}></div>
+
+          <div className={styles.searchContainer}>
+            <div className={styles.title}>
+              <h1 style={{cursor:"default"}}>adnan-yusuf.com</h1>
+            </div>
+            <div className={styles.searchbar}>
+              <div className={styles.bar} style={{cursor:"default", borderBottom: '1px solid black'}}>
+                Adnan Yusuf's
+              </div>
+            </div>
+          </div>
+
+
+            <div className={styles.photoContainer}>
+              <img src="/profpic.jpg" alt="Adnan Yusuf" style={{width: '50%'}}/>
+            </div>
+
       </div>
-      <div className={styles.title}>
-        <h1 style={{cursor:"default"}}>Adnan Yusuf</h1>
-      </div>
-      <div className={styles.searchbar}></div>
+
+      <footer style={{position: 'fixed', bottom:'0', left: '0', margin:'1vh', fontFamily:'Roboto, sans-serif', fontWeight:'100', fontSize:'1vw'}}>
+        Adnan's Personal Website v2.0.0</footer>
 
     </div>
   )
